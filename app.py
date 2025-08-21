c14e148357f33859affacbd41760839846493ff0
Swapneel Giri <swapneelgiri5@gmail.com>
2025-08-21 15:42:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import streamlit as st
 from sentence_transformers import SentenceTransformer
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 
+#Loading Datasets
 with open('ipc_qa.json') as f1, open('crpc_qa.json') as f2:
     ipc_data = json.load(f1)
     crpc_data = json.load(f2)
@@ -13,6 +14,7 @@ with open('ipc_qa.json') as f1, open('crpc_qa.json') as f2:
 documents = ipc_data + crpc_data
 corpus = [doc['question'] + " " + doc['answer'] for doc in documents]
 
+#Embedding Model
 @st.cache_resource
 def load_embedder():
     return SentenceTransformer('all-MiniLM-L6-v2')
@@ -20,10 +22,12 @@ def load_embedder():
 embedder = load_embedder()
 corpus_embeddings = embedder.encode(corpus, convert_to_tensor=False)
 
+#FAISS Indexing
 dimension = len(corpus_embeddings[0])
 index = faiss.IndexFlatL2(dimension)
 index.add(np.array(corpus_embeddings))
 
+# Q&A Model function
 @st.cache_resource
 def load_qa_model():
     model_name = "google/flan-t5-base"
@@ -38,10 +42,13 @@ def load_qa_model():
 
 qa_model = load_qa_model()
 
+#Function for retrieval
 def retrieve(query, k=3):
     query_vec = embedder.encode([query])
     distances, indices = index.search(np.array(query_vec), k)
     return [corpus[i] for i in indices[0]]
+
+#Function for answer generation
 def generate_answer(query):
     retrieved_docs = retrieve(query)
     context = "\n".join(retrieved_docs)
@@ -49,6 +56,7 @@ def generate_answer(query):
     response = qa_model(prompt, max_new_tokens=200)[0]['generated_text']
     return response.strip()
 
+#App deployment using STREAMLIT
 st.set_page_config(page_title="⚖️ AI-Powered Legal Assistant", layout="centered")
 st.title("⚖️ AI-Powered Legal Assistant")
 st.write("Ask any legal question and get an AI-generated answer")
@@ -61,4 +69,5 @@ if st.button("Get Answer"):
         st.subheader("Answer:")
         st.write(answer)
     else:
+
         st.warning("Please enter a valid question before submitting.")
